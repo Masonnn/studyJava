d314a24f255da9abb310c73800498b69a24bbcc2
Mason <lixq@weilaicheng.com>
2019-10-24 19:23:29 +0800

    36

---
+++ src/Lesson73_extends/_36/UsePhoneExtendsMerchandise.java
@@ -0,0 +1,23 @@
+package Lesson73_extends._36;
+
+import Lesson73_extends._36.supermarket.Phone;
+
+public class UsePhoneExtendsMerchandise {
+    public static void main(String[] args) {
+        Phone phone = new Phone(
+                "手机001", "Phone001", 100, 1999, 999,
+                4.5, 3.5, 4, 128, "索尼", "安卓"
+        );
+
+        phone.describe();
+        System.out.println();
+
+        System.out.println(phone.getName());
+        System.out.println();
+
+        phone.buy(100);
+
+        phone.buy(3);
+
+    }
+}
