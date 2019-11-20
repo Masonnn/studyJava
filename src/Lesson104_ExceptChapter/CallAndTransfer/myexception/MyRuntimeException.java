a8c3fbc8c2be8edd0385576885205a63b8cf0f06
Mason <lixq@weilaicheng.com>
2019-11-20 20:53:00 +0800

    my exception

---
+++ src/Lesson104_ExceptChapter/CallAndTransfer/myexception/MyRuntimeException.java
@@ -0,0 +1,19 @@
+package Lesson104_ExceptChapter.CallAndTransfer.myexception;
+
+public class MyRuntimeException extends RuntimeException {
+    public MyRuntimeException() {
+    }
+
+    public MyRuntimeException(String message) {
+        super(message);
+    }
+
+    public MyRuntimeException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public MyRuntimeException(Throwable cause) {
+        super(cause);
+    }
+
+}
