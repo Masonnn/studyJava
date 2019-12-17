0d18fe769b89051e806e974fcd8ad05c2df2743f
Mason <lixq@weilaicheng.com>
2019-12-17 17:03:10 +0800

    bugfix

--- src/Chapter4/CollectionList/mylist/MyLinkedList.java
+++ src/Chapter4/CollectionList/mylist/MyLinkedList.java
@@ -1,11 +1,6 @@
 package Chapter4.CollectionList.mylist;
 
 import java.util.*;
-//import java.util.function.Consumer;
-//import java.util.function.IntFunction;
-//import java.util.function.Predicate;
-//import java.util.function.UnaryOperator;
-//import java.util.stream.Stream;
 
 public class MyLinkedList implements List {
 
