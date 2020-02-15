293e4e34ec3e8bc1ab8c8f25d705384d5f446833
Mason <lixq@weilaicheng.com>
2020-02-15 11:35:57 +0800

    save

--- src/test.java
+++ src/test.java
@@ -2,29 +2,25 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class test {
-//    private List<String> result;
-//    public List<String> generateParentheses(int n) {
-//        result = new ArrayList<String>();
-//        recur(0,0,n,"");
-//        return result;
-//    }
-//
-//    public void recur(int left, int right, int n, String s) {
-//        if (left == n && right == n) {
-//            result.add(s);
-//        }
-//
-//        if (left < n) {
-//            recur(left + 1, right, n, s + "(");
-//        }
-//        if (right < left) {
-//            recur(left, right + 1, n, s + ")");
-//        }
-//
-//    }
+    public int maxSubArray(int[] nums) {
+        int maxSum = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i-1] > 0){
+                nums[i] = nums[i] + nums[i-1];
+            }
+            maxSum = Math.max(nums[i], maxSum);
+        }
+        System.out.println(maxSum);
+        return maxSum;
+    }
 
     public static void main(String[] args) {
-        List<List<Integer>> triangle = { };
+
+        int[] nums = new int[]{-2,1,-3,4,-1,2,1,-5,4};
+
+        test test = new test();
+
+        test.maxSubArray(nums);
 
     }
 }
